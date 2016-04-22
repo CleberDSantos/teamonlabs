b88d1c3d9fe751e91971d4527f73bbf15c4a29f8
CleberDSantos <systemofcleber@gmail.com>
2016-04-21 23:11:36 -0300

    Add project files.

---
+++ src/TeamOnLabs.Timeline.UI/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace TeamOnLabs.Timeline.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
